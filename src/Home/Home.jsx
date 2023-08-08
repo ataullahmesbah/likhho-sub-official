d6e9ec64813a2e9d77fa00f7980eb590b3ecaa66
Mou <mahejabinmou.99@gmail.com>
2023-08-08 11:57:29 -0700

    Home component created

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="bg-green-200">Mou</h2>
+        </div>
+    );
+};
+
+export default Navbar;
