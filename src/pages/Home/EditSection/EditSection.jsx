4b8a1a4a4f512ed1046ceb116d2ac4040b1d14f3
acchayon <adhikaryac81@gmail.com>
2023-08-10 00:38:27 +0600

    added by ac chayon

---
+++ src/pages/Home/EditSection/EditSection.jsx
@@ -0,0 +1,17 @@
+import PicPart from "./PicPart";
+import WritePart from "./WritePart";
+
+const EditSection = () => {
+    return (
+        <div className="mt-10">
+            <h2 className="text-center px-4 mb-5 text-3xl font-bold">Edit documents with LIkho</h2>
+            
+            <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center">
+                <PicPart></PicPart>
+                <WritePart></WritePart>
+            </div>
+        </div>
+    );
+};
+
+export default EditSection;
