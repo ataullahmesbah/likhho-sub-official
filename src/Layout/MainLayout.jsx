5b92bd32dd69e1c170935364b1a859c8d903fd3a
arbayezid <arbayezid0@gmail.com>
2023-08-09 15:06:00 +0600

    footer added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../pages/shared/Footer";
 
 
 const MainLayout = () => {
@@ -7,6 +8,7 @@ const MainLayout = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/shared/Footer.jsx
@@ -0,0 +1,47 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <h3 className="text-3xl">Likho</h3>
+            </div>
+            <div>
+                <span className="footer-title">Product</span>
+                <a className="link link-hover">Features</a>
+                <a className="link link-hover">Pricing</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Solutions</span>
+                <a className="link link-hover">Overview</a>
+                <a className="link link-hover">Tech</a>
+                <a className="link link-hover">Government</a>
+                <a className="link link-hover">Non-Profit</a>
+                <a className="link link-hover">Financial Services</a>
+            </div>
+            <div>
+                <span className="footer-title">Customers</span>
+                <a className="link link-hover">Highlights</a>
+                <a className="link link-hover">Case Studies</a>
+            </div>
+            <div>
+                <span className="footer-title">Resources</span>
+                <a className="link link-hover">Documentation</a>
+                <a className="link link-hover">eBooks</a>
+                <a className="link link-hover">Podcast</a>
+                <a className="link link-hover">Blog</a>
+                <a className="link link-hover">Resources</a>
+            </div>
+            <div>
+                <span className="footer-title">About Us</span>
+                <a className="link link-hover">Our Company</a>
+                <a className="link link-hover">Careers</a>
+                <a className="link link-hover">Contact Us</a>
+                <a className="link link-hover">News</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
