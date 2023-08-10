9f319d6d36463135f892bce2da2eb66593304c98
acchayon <adhikaryac81@gmail.com>
2023-08-10 18:48:41 +0600

    change some pic and update ui edit and template section

--- src/pages/Home/Template/TemplateSection.jsx
+++ src/pages/Home/Template/TemplateSection.jsx
@@ -1,19 +1,47 @@
 
-
 const TemplateSection = () => {
+    const containerStyle = {
+        maxHeight: '400px',
+        overflowY: 'auto',
+    };
+
     return (
-        <div className="mt-5">
-            <h2 className="text-center text-3xl font-bold">Our Popular Template</h2>
+        <div className="mt-20">
+            <h2 className="text-center text-4xl text-blue-950 font-bold">Our Popular Template</h2>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 mt-5">
-                <div className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105">
-                    <img className="w-full" src="https://i.ibb.co/4RdVCYf/template1.jpg" alt="" />
+                <div
+                    className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105 relative"
+                    style={containerStyle}
+                >
+                    <img
+                        className="w-full"
+                        src="https://i.ibb.co/h181Qwg/tem2.webp"
+                        alt=""
+                    />
+                    <div className="absolute inset-0 bg-black opacity-0 hover:opacity-30 transition duration-300"></div>
                 </div>
-                <div className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105">
-                    <img className="w-full" src="https://i.ibb.co/rZ6b0cH/template2.jpg" alt="" />
+                <div
+                    className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105 relative"
+                    style={containerStyle}
+                >
+                    <img
+                        className="w-full"
+                        src="https://i.ibb.co/VJQTMX1/tem3.png"
+                        alt=""
+                    />
+                    <div className="absolute inset-0 bg-black opacity-0 hover:opacity-30 transition duration-300"></div>
                 </div>
-                <div className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105">
-                    <img className="w-full" src="https://i.ibb.co/jVHGjM5/template3.jpg" alt="" />
+                <div
+                    className="rounded-lg overflow-hidden transition duration-300 transform hover:scale-105 relative"
+                    style={containerStyle}
+                >
+                    <img
+                        className="w-full"
+                        src="https://i.ibb.co/ZBFXd68/tem1.webp"
+                        alt=""
+                    />
+                    <div className="absolute inset-0 bg-black opacity-0 hover:opacity-30 transition duration-300"></div>
                 </div>
             </div>
         </div>
