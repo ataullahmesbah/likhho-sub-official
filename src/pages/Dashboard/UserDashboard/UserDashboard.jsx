a66b3c7ee6002621e02642159ebf6407b82ae841
himaloyash <himaloyashis98@gmail.com>
2023-08-17 01:58:57 +0600

    dashboard added

--- src/pages/Dashboard/UserDashboard/UserDashboard.jsx
+++ src/pages/Dashboard/UserDashboard/UserDashboard.jsx
@@ -1,11 +1,99 @@
-import React from 'react';
 
-const UserDashboard = () => {
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Account from '../../Account/Account';
+import { BiFullscreen } from 'react-icons/bi';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
     return (
-        <div>
-            
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
         </div>
     );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
-export default UserDashboard;
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+export default function UserDashboard() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <>
+            <Account></Account>
+            <Box
+                sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', width: "BiFullscreen", height: 524 }}
+            >
+                <Tabs
+                    orientation="vertical"
+                    // variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                // aria-label="Vertical tabs example"
+                // sx={{ borderRight: 1, borderColor: 'divider' }}
+                >
+
+                    <Tab label="Account" {...a11yProps(0)} />
+                    <Tab label="Inbox" {...a11yProps(1)} />
+                    <Tab label="Sent" {...a11yProps(2)} />
+                    <Tab label="Save Document" {...a11yProps(3)} />
+                    <Tab label="Item Five" {...a11yProps(4)} />
+                    <Tab label="Item Six" {...a11yProps(5)} />
+                    <Tab label="Item Seven" {...a11yProps(6)} />
+                    <Tab label="Item Seven" {...a11yProps(7)} />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    <image src="https://img.freepik.com/free-photo/bohemian-man-with-his-arms-crossed_1368-3542.jpg?size=626&ext=jpg&ga=GA1.2.1842592228.1683275805&semt=sph"></image>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                    Item Four
+                </TabPanel>
+                <TabPanel value={value} index={4}>
+                    Item Five
+                </TabPanel>
+                <TabPanel value={value} index={5}>
+                    Item Six
+                </TabPanel>
+                <TabPanel value={value} index={6}>
+                    Item Seven
+                </TabPanel>
+            </Box>
+        </>
+    );
+}
