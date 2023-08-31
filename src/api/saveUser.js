86e3c7a578994bb4b9f7ea0e0953fa65a54c1deb
himaloyash <himaloyashis98@gmail.com>
2023-08-31 19:36:18 +0600

    sent dashboard added

--- src/api/saveUser.js
+++ src/api/saveUser.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React from 'react';
 
 const saveUser = (user) => {
     console.log(user)
--- src/pages/Dashboard/DashBoardSent/DashBoardSent.jsx
+++ src/pages/Dashboard/DashBoardSent/DashBoardSent.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BsFillPenFill } from "react-icons/bs";
 
 const DashBoardSent = () => {
     return (
         <div>
-            
+            <div>
+                <h1>Dochub Inbox</h1>
+                <p>Your inbox contains documents that require your action in order to complete them.</p>
+            </div>
+            <div>
+                <div>
+                    <p className='inline-flex'> <BsFillPenFill></BsFillPenFill> Sign request</p>
+                </div>
+                <div></div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };
